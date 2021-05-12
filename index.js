a428027c43d1affa104b0ff1d316465ae8d43a04
Eduardo Chavez PC <eduardo.cdorado@alumnos.udg.mx>
2021-05-12 04:39:52 -0500

    variable var

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-/* 
-let miNombre = new String("Lalo")
-let miDia = new Date(2021, 12, 8)
+myName = 'Chris';
 
-if(miNombre instanceof String){
-    console.log("miNombre si es una instancia del Objeto String",miNombre instanceof Object);
+function logName() {
+  console.log(myName);
 }
 
-if(miDia instanceof Date){
-    console.log("miDia si es una instancia del Objeto Date",miDia instanceof Object);
-}
-*/
+logName();
 
-function Coche(fabricante, modelo, ejercicio) {
-    this.fabricante = fabricante
-    this.modelo = modelo
-    this.ejercicio= ejercicio
- }
-
- miCoche = new Coche("Honda", "Accord", 1998)
- a = miCoche instanceof Coche // devuelve verdadero (true)
- b = miCoche instanceof Object // devuelve verdadero (true)
-
- console.log(a);
- console.log(b);
-
+var myName;
