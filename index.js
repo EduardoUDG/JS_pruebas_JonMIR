6704064150dcc8999571fcbd7013a3782f016a87
Eduardo Chavez PC <eduardo.cdorado@alumnos.udg.mx>
2021-05-17 16:07:50 -0500

    Booleans

--- index.js
+++ index.js
@@ -1,48 +1,20 @@
 
-
-let a = new Number('123'); // a === 123 es false
-let b = Number('123'); // b === 123 es true
-
-// console.log(`"a" es igual === 123?  ${a === 123}`)
-// console.log(`"b" es igual === 123?  ${b === 123}`)
-
-// console.log(`"a" es una unstancia de Number? `, a instanceof Number) // es true
-// console.log(`"b" es una instancia de Number? `, b  instanceof Number) // es false
+const prueBool1 = 0            // false
+const prueBool2 = -0           // false
+const prueBool3 = null         // false
+const prueBool4 = undefined    // false
+const prueBool5 = ""           // false
 
 
-// ******************************************************************************
 
-let decimal = 7.19
-
-// console.log(decimal.toFixed(1))
-// console.log(decimal.toFixed(3))
-
-
-// ******************************************************************************
+const pruebaObj = {}           // true    
+const pruebaArr = []           // true
+const pruebaCad1 = "false"     // true
+const pruebaCad2 = "true"      // true
+const pruebaCad3 = -10         // true
+const pruebaCad4 = " "      // true
 
 
-let numberOne = 15.80
-let numberText = '20.5'
-
-let parteFlotante1 = parseFloat(numberOne)
-let parteFlotante2 = parseFloat(numberText)
-
-// solo nos devuelve  la parte entera
-// console.log(parseInt(numberOne))
-// console.log(parseInt(numberText))
 
 
-// ******************************************************************************
 
-
-const MasgrandeNum    = Number.MAX_VALUE;
-const MaspequeNum     = Number.MIN_VALUE;
-const infinitoNum     = Number.POSITIVE_INFINITY;
-const notInfinitoNum  = Number.NEGATIVE_INFINITY;
-const noEsNum         = Number.NaN;
-
-console.log(MasgrandeNum)
-console.log(MaspequeNum)
-console.log(infinitoNum)
-console.log(notInfinitoNum)
-console.log(noEsNum)
