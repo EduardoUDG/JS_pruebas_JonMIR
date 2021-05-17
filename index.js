fa9a25ba85d1a43d6a44fbce280b8c4930bfb9f6
Eduardo Chavez PC <eduardo.cdorado@alumnos.udg.mx>
2021-05-17 08:53:58 -0500

    Cadenas de texto (String)

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
-myName = 'Chris';
+let wordLength = 'Este es un pequeño parrafo para saber cuantos caracteres tiene'
+// console.log(wordLength.length)
 
-function logName() {
-  console.log(myName);
-}
 
-logName();
+// ***************************************************
 
-var myName;
+
+let words = `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+Commodi veniam ab exercitationem expedita distinctio numquam
+facilis eos placeat ullam deleniti possimus ea quo, saepe sunt?
+Necessitatibus itaque cumque praesentium sed`
+
+let word = 'Eduardo'
+// console.log(`The word "${word}" ${ words.includes(word) ? 'is' : 'is not'  } in the sentence`);
+
+
+// ***************************************************
+
+
+let userName = 'Eduardo Chavez Dorado'
+// console.log(userName.toLowerCase())
+// console.log(userName.toUpperCase())
+
+
+// ***************************************************
+
+let password = `   password    `
+// console.log(password)
+// console.log(password.trim())
+
+
+// ***************************************************
+
+
+const namesList = 'Lalo Hector Juan Juanca' 
+let separador = " "
+let arrayList = namesList.split(separador)
+
+// console.log(arrayList instanceof Array)
+arrayList.forEach(element => console.log(`Printing element: ${element}`))
+
+
+
