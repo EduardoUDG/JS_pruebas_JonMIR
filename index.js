e0c5f725a94f404a8ca08ab0fb22334d07664516
Eduardo Chavez PC <eduardo.cdorado@alumnos.udg.mx>
2021-05-17 12:22:09 -0500

    Template string (backticks)

--- index.js
+++ index.js
@@ -1,43 +1,8 @@
-let wordLength = 'Este es un pequeño parrafo para saber cuantos caracteres tiene'
-// console.log(wordLength.length)
 
 
-// ***************************************************
+let firstName = 'Eduardo'
+let lastName  = 'Chavez'
 
+const greetings = `Welcome ${firstName} ${lastName}, we have novelties for you`
 
-let words = `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-Commodi veniam ab exercitationem expedita distinctio numquam
-facilis eos placeat ullam deleniti possimus ea quo, saepe sunt?
-Necessitatibus itaque cumque praesentium sed`
-
-let word = 'Eduardo'
-// console.log(`The word "${word}" ${ words.includes(word) ? 'is' : 'is not'  } in the sentence`);
-
-
-// ***************************************************
-
-
-let userName = 'Eduardo Chavez Dorado'
-// console.log(userName.toLowerCase())
-// console.log(userName.toUpperCase())
-
-
-// ***************************************************
-
-let password = `   password    `
-// console.log(password)
-// console.log(password.trim())
-
-
-// ***************************************************
-
-
-const namesList = 'Lalo Hector Juan Juanca' 
-let separador = " "
-let arrayList = namesList.split(separador)
-
-// console.log(arrayList instanceof Array)
-arrayList.forEach(element => console.log(`Printing element: ${element}`))
-
-
-
+console.log(greetings)
