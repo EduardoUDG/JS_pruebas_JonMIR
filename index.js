bcacf7c52ca1253ff6982e91f36dfa99445f5993
Eduardo Chavez PC <eduardo.cdorado@alumnos.udg.mx>
2021-05-17 16:25:21 -0500

    null, NaN & undefined

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 
-const prueBool1 = 0            // false
-const prueBool2 = -0           // false
-const prueBool3 = null         // false
-const prueBool4 = undefined    // false
-const prueBool5 = ""           // false
 
+let indefinida
+let variable = null
 
-
-const pruebaObj = {}           // true    
-const pruebaArr = []           // true
-const pruebaCad1 = "false"     // true
-const pruebaCad2 = "true"      // true
-const pruebaCad3 = -10         // true
-const pruebaCad4 = " "      // true
-
-
-
-
-
+console.log(indefinida) // undefined
+console.log(variable)   // null
