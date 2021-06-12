32ab7a21123bc6573bb1c2d651285fa91521f092
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2021-06-12 13:54:51 -0500

    Objetos MDN parte1

--- index.js
+++ index.js
@@ -1,60 +1,183 @@
+// const object1 = {
+// 	a:'foo',
+// 	b:42,
+// 	c:{}
+// };
+// object1.a = "KFC"
+// console.log(object1.a)
 
 
-// Como definir objetos en JS
- 
-const Obj = {
-	
-};
-console.log(Obj)
+// const a = 'foo'
+// const b = 42
+// const c = {}
+// const object2 = { 
+// 	a:a,
+// 	b:b,
+// 	c:c
+// };
+// object2.b = 20
+// console.log(object2)
 
 
-const o = new Object();
-console.log(o)
+// const object3 = {
+// 	a,
+// 	b,
+// 	c
+// };
+// object3.a = 'Hamburguesa'
 
-
-
-
-const Person = {
-	name:'Eduardo',
-	lastName: 'Chavez',
-	age:20,
-	hobbies:['Ejercicio', 'Musica', 'Video Juegos', 'Aprender'],
-	status: false,
-	contact:{
-		email:'eduardo@gmail.com',
-		twitter:'@LaloEnchilado',
-		movil: 522212345678
-	},
-	saludar:function(){
-		console.log('Hola :D');
-	},
-	sayMyName:function(){
-		console.log(`Hi my name is ${this.name} ${this.lastName} and I'm twenty years old`);
-	}
-}
-// console.log(Person)
-console.log(Person.name)
-console.log(Person['lastName'])
-console.log(Person['age'])
-console.log(Person.contact.twitter)
-Person.saludar()
-Person.sayMyName()
-console.log(Object.keys(Person))
-console.log(Object.values(Person))
-console.log(Object.hasOwnProperty("name"))
-console.log(Object.hasOwnProperty("Edad"))
+// console.log(object3)
 
 
 
 
+//* Nuevas notaciones en ECMAScript 2015
+
+// ///? para declarar propiedades/valor
+// let a = 'foo', b = 42, c = {};
+// let Object1 = {
+// 	a,
+// 	b,
+// 	c
+// };
+// console.log(Object1);
+
+
+
+// //? para declara metodos/funciones
+// const object2 = {
+// 	name:'Eduardo',
+// 	metodoSaludar(){
+// 		console.log(`Hi ${this.name}`)
+// 	},
+// };
+// object2.metodoSaludar()
+
+
+
+// //? declara nombre de propiedades
+// let food = 'banana'
+
+// const object3 = {
+// 	[food]:"I'm a banana",
+// 	['b' + 'ar']:'there'
+// };
+// console.log(object3)
+
+
+
+
+//* Accediendo a propiedades
+// let name = 'Eduardo' 
+// const Objeto = {
+// 	name,
+// 	age:20,
+// 	saludar(){
+// 		console.log(`Hi ${this.name}`)
+// 	}
+// };
+
+// console.log(Objeto['name']);
+// console.log(Objeto.age);
+// Objeto.saludar()
+
+// console.log(Objeto.name === {name}.name)
+// const Objeto = { x:1, x:2 }
+// console.log(Objeto.x)
 
 
 
 
 
+//* Funcion generadora
+
+// async function* request(){
+// 	let i = 1;
+// 	let url = "http://jsonplaceholder.typicode.com/photos/";
+
+// 	while(true){
+// 		let resp = await fetch(url+i);
+// 		let json = await resp.json();
+		
+// 		yield json;
+// 		i++;
+// 	}
+// }
+
+// let r  = request()
+// async function get(){
+// 	let content = await r.next()
+// 	let imageUrl = content.value.url;
+// 	let template = `
+// 	<li>
+// 		<img src="${imageUrl}" width="200px"></img>
+// 	</li>`;
+// 	document.getElementById('miDiv').innerHTML += template; 
+// }
+
+
+
+// function* serie() {
+// 	yield 1;
+// 	yield 10;
+// 	yield 100;
+// 	yield 1000;
+// 	yield 10000;
+// }
+
+
+// const iterationFuncion = serie(); // Funcion iteradora / Array iteration
+// console.log(iterationFuncion.next())
+
+// function* subSerie(){
+// 	yield 'Hola';
+// 	yield 'Soy otra funcion generadora';
+// }
+
+// function* serie(i){
+// 	yield i;
+// 	yield i + 5;
+// 	yield* subSerie();
+// 	yield i + 10;
+// 	yield i + 20;
+// }
+// const iteration = serie(50);
+// console.log(iteration.next());
+// console.log(iteration.next());
+// console.log(iteration.next());
+// console.log(iteration.next());
+// console.log(iteration.next());
+// console.log(iteration.next());
+
+
+
+// const items = ['A', 'B', 'C'];
+// const Obj = {
+// 	[items]:'Hello'
+// };
+
+// console.log(Obj);
+
+
+
+// let param = 'size'
+// let config = {
+//   [param]: 12,
+//   ['mobile' + param.charAt(0).toUpperCase() + param.slice(1)]: 4
+// }
+// console.log(config)
 
 
 
 
+// let obj1 = { foo: 'bar', x: 42 }
+// let obj2 = { foo: 'baz', y: 13 }
+
+// let clonedObj = { ...obj1 };
+// clonedObj.meal = 'Hamburguesa';
+
+// console.log(obj1);
+// console.log(clonedObj);
 
 
+//******************* METODOS  ************************
