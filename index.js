29e47c11790b0de93adce3fe475298e88f0802ad
EduardoUDG <eduardo.cdorado@alumnos.udg.mx>
2021-07-21 20:27:04 -0500

    Ciclos (loops)

--- index.js
+++ index.js
@@ -1,44 +1,62 @@
+//* Ciclos ( Loops )
 
-// Condicionales en Javascript
-let age = 17;
-let ageValidate = 18;
-
-//* if
-// if( age >= validateAge ) {
-//     console.log('Me imprimo en consola :D');
+//! while
+let contador = 0;
+// while( contador < 10 ) {
+//     console.log(`While ${contador}`);
+//     contador++;
 // }
 
 
-//* else if
-// if ( age ) {
-//     console.log('Me imprimo en consola :D')
-// } else if ( age <= ageValidate ) {
-//     console.log('Eres menor de edad');
-// } 
+//! do...while
+// do {
+//     console.log(`do while ${contador}`);
+//     contador++;
+// } while ( contador < 10 );
 
 
-//* Operador ternario
-// let success = 'Eres mayor de edad';
-// let error   = 'Eres menor de edad'; 
 
-// ( age >= ageValidate ) ? console.log(success) : console.log(error);
-
-
-//* switch
-let opc = 1;
-
-// switch( opc ){
-//     case 1:
-//         console.log('Caso #1');
-//     break;
-//     case 2:
-//         console.log('Caso #2');
-//     break;
-//     case 3:
-//         console.log('Caso #3');
-//     break;
-//     case 4:
-//         console.log('Caso #4');
-//     break;
-//     default: console.log('Opcion fuera de rango') ;
+//! for
+// for( let i=0; i<10; i++ ) {
+//     console.log(`for ${i}`);
 // }
+
+
+
+//! for in
+// const user = {
+//     nombre: 'Lalo',
+//     edad: 20,
+//     apellido: 'Chavez'
+// }
+// for( const pro in user) {
+//     console.log(`Propiedad: ${pro} de user`);
+// }
+
+
+//! for of
+// let numeros = [1, 2, 3, 4, 5, 6];
+// for( const ele of numeros) {
+//     console.log(`Elemento: ${ele} del areglo`)
+// }
+
+// let cadena = 'Hola mundo';
+// for( const caracter of cadena) {
+//     console.log(`${caracter}`);
+// }
+
+
+//! Curiosidades con objetos
+let obj = {};
+
+let id = 2654984;
+//* La propiedad "id" si tiene comillas
+obj[id] = {
+    user: 'Lalo',
+    country: 'Mexico'
+};
+
+//* La propiedad "age" no tiene comillas 
+obj['age'] = 10;
+
+console.log( obj );
