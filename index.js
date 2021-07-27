b404d0fd5c60e59ce454b96bee4b92cbe148ad3e
EduardoUDG <eduardo.cdorado@alumnos.udg.mx>
2021-07-26 22:03:36 -0500

    Manejo de errores (try...catch)

--- index.js
+++ index.js
@@ -1,62 +1,18 @@
-//* Ciclos ( Loops )
-
-//! while
-let contador = 0;
-// while( contador < 10 ) {
-//     console.log(`While ${contador}`);
-//     contador++;
-// }
 
 
-//! do...while
-// do {
-//     console.log(`do while ${contador}`);
-//     contador++;
-// } while ( contador < 10 );
+try {
 
+    //* En el try se agrega el código a evaluar
+    console.log('Hola soy un try');
+    noExisto;
+} catch( err ) {
 
+    //! Si hay un error catch captura cualquier error por try
+    console.log( err );    
+} finally {
 
-//! for
-// for( let i=0; i<10; i++ ) {
-//     console.log(`for ${i}`);
-// }
+    //? Finally se ejecutara siempre la final de un bloque try...catch
+    console.log('Hola soy un finally');
+}
 
-
-
-//! for in
-// const user = {
-//     nombre: 'Lalo',
-//     edad: 20,
-//     apellido: 'Chavez'
-// }
-// for( const pro in user) {
-//     console.log(`Propiedad: ${pro} de user`);
-// }
-
-
-//! for of
-// let numeros = [1, 2, 3, 4, 5, 6];
-// for( const ele of numeros) {
-//     console.log(`Elemento: ${ele} del areglo`)
-// }
-
-// let cadena = 'Hola mundo';
-// for( const caracter of cadena) {
-//     console.log(`${caracter}`);
-// }
-
-
-//! Curiosidades con objetos
-let obj = {};
-
-let id = 2654984;
-//* La propiedad "id" si tiene comillas
-obj[id] = {
-    user: 'Lalo',
-    country: 'Mexico'
-};
-
-//* La propiedad "age" no tiene comillas 
-obj['age'] = 10;
-
-console.log( obj );
+//* Usualemnte podemos usar este tema para funciónes async-await
